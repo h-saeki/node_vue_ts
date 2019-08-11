b4f0dc09e22b33cec2e34f561427b4bd25477047
Hisafumi Saeki <saeki03@gmail.com>
2019-08-11 23:21:18 +0900

    FIX

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     'plugin:vue/recommended',
     //'prettier/@typescript-eslint',
     //'plugin:prettier/recommended',
-    "eslint:recommended",
   ],
   rules: {
     // note you must disable the base rule as it can report incorrect errors
